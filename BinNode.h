5e23c7821d77e98e272351eba709b4c63fdb3928
Maxim <wainermaxim@gmail.com>
2022-11-24 23:36:46 +0200

    uppp

--- BinNode.h
+++ BinNode.h
@@ -5,7 +5,7 @@
 #include "Node.h"
 
 template<class T>
-class BinNode : public Node
+class BinNode : public Node<T>
 {
 private:
     Node<T> *m_right;
@@ -14,20 +14,81 @@ private:
     int m_height;
 
 public:
-    BinNode();
+    BinNode(const T& value);
     ~BinNode();
+    BinNode<T> getLeft();
+    BinNode<T> getRight();
+    BinNode<T> getFather();
+    int getHeight();
+
+    void setLeft();
+    void setRight();
+    void setFather();
+    void setHeight();
+
 };
 
 template<class T>
-BinNode<T>::BinNode()
+BinNode<T>::BinNode(const T& value): m_data(value)
 {
-
+    this->m_father=nullptr;
+    this->m_left=nullptr;
+    this->m_right=nullptr;
+    this->height=0
 }
 
 template<class T>
 BinNode<T>::~BinNode()
 {
+    delete this->m_data;
+}
 
+template <class T>
+BinNode<T> getLeft()
+{
+    return this->m_left;
+}
+
+template <class T>
+BinNode<T> getRight()
+{
+    return this->m_right;
+}
+
+template <class T>
+BinNode<T> getFather()
+{
+    return this->m_father;
+}
+
+template <class T>
+int getHeight()
+{
+    return this->m_height;
+}
+
+template <class T>
+void setLeft(T* newLeft)
+{
+    this->m_left = newLeft;
+}
+
+template <class T>
+void setRight(T* newRight)
+{
+    this->m_right=newRight;
+}
+
+template <class T>
+void setFather(T* newFather)
+{
+    this->m_father = newFather;
+}
+
+template <class T>
+void setHeight(int newHeight)
+{
+    this->m_height = newHeight;
 }
 
 #endif //EX1_BINNODE_H
